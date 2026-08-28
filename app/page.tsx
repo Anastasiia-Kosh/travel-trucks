52fe350d45d778b059171ee7dd6300a3f0725a4f
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-08-28 07:35:10 +0300

    edit  responsive design for home and  catalog

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home",
+  title: "TravelTrucks",
   description:
     "Rent a comfortable camper and start your next journey with TravelTrucks.",
 };
